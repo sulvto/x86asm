cd633caf2c468c4c61c28f633bfb436fbeb83396
sulvto <lpetrel@outlook.com>
2017-05-30 22:57:35 +0800

    update c8/run.sh

--- c8/run.sh
+++ c8/run.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+
+# nasm
 nasm c8.asm -o c8.bin
 nasm c8_mbr.asm -o mbr.bin
 
-dd if=mbr.bin of=../a.img bs=512 count=1 conv=notrunc
+dd if=mbr.bin of=../c.img bs=512 count=1 conv=notrunc
 
-dd if=c8.bin of=../a.img bs=512 count=40 seek=100 conv=notrunc
+dd if=c8.bin of=../c.img bs=512 count=40 seek=100 conv=notrunc
+
+# run bochs
+bochs -q -f .bochsrc
