00c7c0a33918628c0250638f32a67ed673a6a485
sulvto <lpetrel@outlook.com>
2017-07-01 08:15:54 +0800

    c15/run.sh

--- c15/run.sh
+++ c15/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # nasm to bin
-nasm ../c13/c13_mbr.asm -o ../c13/mbr.bin
+nasm ../c13/c13_mbr.asm -o mbr.bin
 
-nasm c15_core.asm -o core.bin
+nasm -f bin c15_core.asm -o core.bin
 
-nasm c15.asm -o c.bin
+nasm -f bin c15.asm -o c.bin
 
 
 # dd to a.img
-dd if=../c13/mbr.bin of=../c.img bs=512 count=1 conv=notrunc
+dd if=mbr.bin of=../c.img bs=512 count=1 conv=notrunc
 
-dd if=core.bin of=../c.img bs=512 count=10 seek=1 conv=notrunc
+dd if=core.bin of=../c.img bs=512 count=49 seek=1 conv=notrunc
 
 dd if=c.bin of=../c.img bs=512 count=50 seek=50 conv=notrunc
 
