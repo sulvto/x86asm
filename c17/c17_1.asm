04488a1b8e8d393d43298422ba70706bd40bf640
sulvto <lpetrel@outlook.com>
2017-07-16 08:37:21 +0800

    add c17/c17_1.asm

--- c17/c17_1.asm
+++ c17/c17_1.asm
@@ -1,1 +1,48 @@
 ; x86汇编语言：从实模式到保护模式
+        program_len     dd  program_end
+        entry_point     dd  start
+        salt_position   dd  salt_begin
+        salt_items      dd  (salt_end-salt_begin)/256
+
+;---------------------------------------------------------------------
+
+        salt_begin:
+        PrintString     db  '@PrintString'
+                    times 256-($-PrintString) db 0
+
+        TerminateProgram     db  '@TerminateProgram'
+                    times 256-($-TerminateProgram) db 0
+
+        ReadDiskData     db  '@ReadDiskData'
+                    times 256-($-ReadDiskData) db 0
+
+
+        PrintDWordAsHex     db  '@PrintDWordAsHex'
+                    times 256-($-PrintDWordAsHex) db 0
+
+        salt_end:
+
+        message_0           db  '   User task a->;;;;;;;;;;;;;;;;;;;'
+                            db  0x0d,0x0a,0
+
+;---------------------------------------------------------------------
+
+        [bits 32]
+
+;---------------------------------------------------------------------
+
+start:
+        mov ebx,message_0
+        call far [PrintString]
+        jmp start
+        
+        call far [TerminateProgram]
+
+;---------------------------------------------------------------------
+program_end:
+
+
+
+
+
+
